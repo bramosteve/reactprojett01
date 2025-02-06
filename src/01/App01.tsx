7bcb2024c384f0a717955bb4bd5595f3a7ea0e09
bramosteve <160427659+bramosteve@users.noreply.github.com>
2025-02-06 14:45:26 +0100

    first commit

---
+++ src/01/App01.tsx
@@ -0,0 +1,17 @@
+//mon premier composant
+
+import logo  from './image.jpg'
+
+export default function App01(){
+    return(
+
+
+        
+        <div className="steve" >
+            <div>
+                <img src={logo} alt="" width={500} />
+            </div>
+           mon premier composant 
+        </div>
+    )
+}
