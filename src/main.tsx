ae5b2165e24a2685fe3e78b1ac256f84a05002ad
bramosteve <160427659+bramosteve@users.noreply.github.com>
2025-02-13 18:02:28 +0100

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 //import './01/style.css'
 import { createRoot } from 'react-dom/client'
 
-import App004 from './04/App004.tsx'
+import App0004 from './04/App0004.tsx'
 
 
 
 createRoot(document.getElementById('root')!).render(
   
-    <App004 />
+    <App0004 />
   
 )
